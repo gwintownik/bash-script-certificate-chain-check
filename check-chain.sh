249b4285ac7ee84886d1c37e1ad701c825195032
gwintownik <flanel@wp.pl>
2020-05-18 09:33:50 +0200

    2 commit

---
+++ check-chain.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function parsercert {
+	        CERT=`cat tmp.cert | sed -n $1,$2p`
+	        SUBJECT=`echo -e "$CERT" | grep 'subject=CN = ' | awk -F 'CN =' '{print $2'}`
+	        ISSUER=`echo -e "$CERT" | grep issuer | awk -F 'O =' '{print $2}'`
+	        NOTAFTER=`echo -e "$CERT" | grep notAfter | awk -F '=' '{print $2}'`
+	        MD5=`echo -e "$CERT" | grep MD5 | awk -F '=' '{print $2}'`
+	        echo "$i ; $SUBJECT ; $ISSUER  ; $MD5 ; $NOTAFTER "
+		}
+
+
+
+for i in `cat system-list.txt` 
+	do
+		echo "" | openssl s_client -showcerts -connect $i:443 2>&1 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p;/-END CERTIFICATE-/a\\x0' | sed -e '$ d' | xargs -0rl -I% sh -c "echo '%' |  openssl x509 -subject -issuer -dates -fingerprint -MD5 -noout"  > tmp.cert
+
+	parsercert 1 5
+	parsercert 6 10
+	parsercert 11 15
+	parsercert 16 20 
+	done 
+	
+
