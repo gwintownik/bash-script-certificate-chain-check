4fa22a5c7303e0443bd011ca5b4b4fd7773423e2
gwintownik <flanel@wp.pl>
2020-05-22 15:33:25 +0200

    Modyfikacja skryptu, dodano parametry: 	-f zaczytaj z plik i sprawdz w pętli 	-h sprawdz hosta

--- check-chain.sh
+++ check-chain.sh
@@ -9,16 +9,49 @@ function parsercert {
 	        echo "$i ; $SUBJECT ; $ISSUER  ; $MD5 ; $NOTAFTER "
 		}
 
+function openssl_connect {
+		echo "" | openssl s_client -showcerts -connect $1:443 2>&1 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p;/-END CERTIFICATE-/a\\x0' | sed -e '$ d' | xargs -0rl -I% sh -c "echo '%' |  openssl x509 -subject -issuer -dates -fingerprint -MD5 -noout"  > tmp.cert
+		parsercert 1 5
+		parsercert 6 10
+		parsercert 11 15
+		parsercert 16 20 
+	}
+
+function usage {
+	echo "usage: [-f file ] | [-h host] | [--help]"
+	}
 
 
-for i in `cat system-list.txt` 
-	do
-		echo "" | openssl s_client -showcerts -connect $i:443 2>&1 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p;/-END CERTIFICATE-/a\\x0' | sed -e '$ d' | xargs -0rl -I% sh -c "echo '%' |  openssl x509 -subject -issuer -dates -fingerprint -MD5 -noout"  > tmp.cert
 
-	parsercert 1 5
-	parsercert 6 10
-	parsercert 11 15
-	parsercert 16 20 
-	done 
-	
+while [ "$1" != "" ]; do
+    case $1 in
+        -f )	filename=$2
+		if [ "$filename" != "" ] 
+		then 
+			for i in `cat $filename` 
+				do 
+				openssl_connect $i
+			done
+		else 
+			echo "ERROR Provide filename"
+			exit 1 
+		fi
+		;;
+        -h )	hostname=$2
+	    	if [ "$hostname" != "" ]
+			then 
+				openssl_connect $hostname
+			else 
+				echo "ERROR Provide hostname or IP"
+                                exit 1
+                        fi
 
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
